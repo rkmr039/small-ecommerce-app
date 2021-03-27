2fd01d9a8fae3f0317a79a44c278b890cea432d4
Rishab Kumar <rkmr039@gmail.com>
2021-03-27 14:56:39 +0530

    AJAX call and response handling

--- js/scripts.js
+++ js/scripts.js
@@ -77,7 +77,7 @@ $(function(){
         console.log('Total::' + total)
         return total.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
     }
-    update_params()
+    // update_params()
     $('#quantity').change(function() {
         update_params()
     })
@@ -91,4 +91,120 @@ $(function(){
         $(this).addClass('selected')
         update_params()
     })
+    // *********************** bind() demo ******************************
+/*     var pokemon = {
+        firstname: 'Pika',
+        lastname: 'Chu ',
+        getPokeName: function() {
+            var fullname = this.firstname + ' ' + this.lastname;
+            return fullname;
+        }
+    };
+    console.log(pokemon.getPokeName())
+    var pokemonName = function() {
+        console.log(this.getPokeName() + 'I choose you!');
+    };
+    
+    var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
+    
+    logPokemon(); // 'Pika Chu I choose you!'
+    console.log(typeof(logPokemon)) */
+
+    // *************************** callback demo *****************************
+
+/*     function get_user(callbackFunction) {
+        const user = {
+            name: "RISHAB"
+        }
+        setTimeout(function() {
+            callbackFunction(user)
+        }, 1000)
+    }
+    function greet_user(user) {
+        console.log('Hi' + user.name + ' How are you doing!')
+    }
+
+    get_user(greet_user)
+    function good_bye(user) {
+        console.log('Goodbye ' + user.name)
+    }
+    get_user(good_bye)
+
+    get_user(function(user) {
+        console.log('Hey ' + user.name + ' this is another way of calling callback function')
+    }) */
+
+    // ******************************** Error Handling ***********************************
+
+
+/*     function get_user2() {
+        const user = {
+            name: ""
+        }
+        setTimeout(function() {
+            return user;
+        }, 1000)
+    }
+    function greet_user2(user) {
+        console.log('Hi' + user.name + ' How are you doing!')
+    }
+
+    try {
+        const user = get_user2()
+        if (!user.name) {
+            throw 'Name is invalid or empty'
+        }
+        greet_user2(user)
+    } catch(err) {
+        console.log('Error occured::' + err)
+        console.log('Hi Guest User How are you doing!')
+    } finally {
+    console.log('============================Finally error is handeled==========================')
+    } */
+
+    // ************************************ Slider ********************************************
+
+
+/*     var bestSellerSlider = {
+        products: ['P1', 'P2', 'P3'],
+        interval: 3000,
+        infinite: false,
+
+        getProducts() {
+            console.log(this.products)
+        }
+    }; */
+
+/*     var startDate = new Date().getTime()
+    for(let i = 0; i < 100000; i++) {
+        console.log(i);
+    }
+    var endDate = new Date().getTime()
+    console.log("Start Time: " + startDate);
+    console.log("End Time: " + endDate); */
+
+    // ***************************  AJAX **********************
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        console.log(this.readyState);
+        if(this.readyState == 4 && this.status == 200) {
+            // console.log(this.responseText);
+            var responseObject = JSON.parse(this.responseText)
+            // console.log(responseObject)
+            var question = responseObject.results[0].question;
+            console.log('Question:: ' + question)
+            var answers = responseObject.results[0].incorrect_answers;
+            answers.push(responseObject.results[0].correct_answer)
+            console.log('Answers:: ', answers)
+           
+        }
+        // console.log(this.responseText);
+    }
+
+    xhttp.open("GET", 'https://opentdb.com/api.php?amount=2')
+
+    xhttp.send()
+
  });
